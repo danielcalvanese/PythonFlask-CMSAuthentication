c883110a956d43779ad3817364544d62a391225a
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-24 08:58:34 -0500

    Added module 1 tasks.

--- cms/admin/__init__.py
+++ cms/admin/__init__.py
@@ -4,15 +4,17 @@ from cms.admin.models import Content, Type, User, Setting, db
 from datetime import datetime
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin', template_folder='templates')
-
+from cms.admin import auth
 
 
 def requested_type(type):
     types = [row.name for row in Type.query.all()]
     return True if type in types else False
 
+
 @admin_bp.route('/', defaults={'type': 'page'})
 @admin_bp.route('/<type>')
+@auth.protected
 def content(type):
     if requested_type(type):
         content = Content.query.join(Type).filter(Type.name == type)
@@ -20,7 +22,9 @@ def content(type):
     else:
         abort(404)
 
+
 @admin_bp.route('/create/<type>', methods=('GET', 'POST'))
+@auth.protected
 def create(type):
     if requested_type(type):
         if request.method == 'POST':
@@ -29,18 +33,18 @@ def create(type):
             type_id = request.form['type_id']
             body = request.form['body']
             error = None
-            
+
             if not title:
                 error = 'Title is required.'
             elif not type_id:
                 error = 'Type is required.'
-                
+
             if error is None:
                 content = Content(title=title, slug=slug, type_id=type_id, body=body)
                 db.session.add(content)
                 db.session.commit()
                 return redirect(url_for('admin.content', type=type))
-        
+
             flash(error)
 
         types = Type.query.all()
@@ -48,7 +52,9 @@ def create(type):
     else:
         abort(404)
 
+
 @admin_bp.route('/edit/<id>', methods=('GET', 'POST'))
+@auth.protected
 def edit(id):
     content = Content.query.get_or_404(id)
     type = Type.query.get(content.type_id)
@@ -61,25 +67,30 @@ def edit(id):
         content.body = request.form['body']
         content.updated_at = datetime.utcnow()
         error = None
-        
+
         if not request.form['title']:
             error = 'Title is required.'
-            
+
         if error is None:
             db.session.commit()
             return redirect(url_for('admin.content', type=type.name))
-    
+
         flash(error)
 
     types = Type.query.all()
-    return render_template('admin/content_form.html', types=types, title='Edit', item_title=content.title, slug=content.slug, type_name=type.name, type_id=content.type_id, body=content.body)
+    return render_template('admin/content_form.html', types=types, title='Edit', item_title=content.title,
+                           slug=content.slug, type_name=type.name, type_id=content.type_id, body=content.body)
+
 
 @admin_bp.route('/users')
+@auth.protected
 def users():
     users = User.query.all()
     return render_template('admin/users.html', title='Users', users=users)
 
+
 @admin_bp.route('/settings')
+@auth.protected
 def settings():
     settings = Setting.query.all()
     return render_template('admin/settings.html', title='Settings', settings=settings)
